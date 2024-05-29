a83aa2464aa882b88f88a864246d280b05be8e4c
Alina Kuznetsova <kuznetsovalina@list.ru>
2024-05-29 17:38:18 +0400

    docs: change a README and add a comments for methods,constants and classes

--- game/road.py
+++ game/road.py
@@ -1,14 +1,25 @@
 import pygame as pg
 
-from helper import load_image
+from helper import load_image, calculate_start_object_position
+
+
+#  start coordinates for first roads
+START_ROAD_X = 160
+START_ROAD_Y = 0
 
 
 class Road(pg.sprite.Sprite):
 
-    def __init__(self, x_position, y_position, *groups):
+    def __init__(self, offset, number_road, *groups):
         pg.sprite.Sprite.__init__(self, *groups)
         self.image = load_image("road.gif")
-        self.rect = self.image.get_rect().move(x_position, y_position)
+        self.number_road = number_road
+        self.offset = offset
 
-        self.rect.x = x_position
-        self.rect.y = y_position
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = calculate_start_object_position(
+            START_ROAD_X,
+            START_ROAD_Y,
+            self.offset,
+            self.number_road
+        )
